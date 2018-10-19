2656c96d9a56bb4adaf5bd848a9b1c7d123572e6
Shajal Ahamed <shajalahamed@Shajals-MacBook-Pro.local>
2018-10-19 21:34:39 +0600

    User CRUD is rocking

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,26 +1,55 @@
 const db  = require('../config/db.config');
 const User = db.users;
-// Post a Customer
+const connection= db.connection;
+// Post a User
 exports.create = (req, res) => {	
 	// Save to MySQL database
-	
-	var user;
+
 	User.create({ 
 	  email: req.body.email,
 	  password: req.body.password
-	}).then(createdCustomer => {		
-		// Send created customer to client
-        customer = createdCustomer;
-        res.send(customer);
+	}).then(user => {		
+		// Send created user to client
+        res.send(user);
     });
 };
  
 // FETCH all Customers include Addresses
 exports.findAll = (req, res) => {
-	User.findAll({
-        attributes: [['uuid', 'userId'], 'email', ['password','pass']],
-        limit : 10
-	}).then((users) => {
-        res.status(200).json({'result': users});
+    User.findAll()
+  .then(users => {
+    // We don't need spread here, since only the results will be returned for select queries
+    res.send(users);
+  });
+};
+
+// FETCH one user
+exports.findOne = (req, res) => {
+    User.findOne(
+        {where : {email : req.params.email}})
+    .then(user => {
+        res.send(user);
     });
 };
+
+exports.updateUserEmail = (req, res) => {
+    User.findOne(
+        {where : {email : req.params.email}}
+    )
+    .then( user =>{
+        user.updateAttributes({
+            email : 'Hi'
+        });
+        res.send(user)
+    });
+};
+
+exports.deleteUser = (req, res) => {
+    User.destroy({
+        where : {email: req.params.email}
+    })
+    .then(deletedUser => {
+        console.log(`Has the Max been deleted? 1 means yes, 0 means no: ${deletedUser}`);
+        res.json({ deleted : 'yes'}).send();
+      });
+}
--- src/route/user.route.js
+++ src/route/user.route.js
@@ -3,9 +3,18 @@ module.exports = function(app) {
  
     const users = require('../controller/user.controller');
  
-    // Create a new Customer
+    // Create a new user
     app.post('/api/users', users.create);
  
-    // Retrieve all Customer
+    // Retrieve all user
     app.get('/api/users', users.findAll);
+
+    //Retrieve one user
+    app.get('/api/user/:email',users.findOne);
+
+    //Update one user
+    app.patch('/api/user/:email', users.updateUserEmail);
+
+    //Delete one user
+    app.delete('/api/user/:email', users.deleteUser);
 }
