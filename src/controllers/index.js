f38426e2e23b11155709ef04f0b11334f4116f3a
Shajal Ahamed <shajalahamed@Shajals-MacBook-Pro.local>
2018-11-27 17:56:32 +0600

    Basic structure of docker is working

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -6,7 +6,6 @@ const repayment = require('./repayment');
 const investor_investment = require('./investorinvestment');
 const active_loan = require('./activeloan');
 const auth_controll = require('./auth');
-const oauth2Facebook = require('./oauth2Facebook');
 
 module.exports = {
     user,
@@ -16,6 +15,5 @@ module.exports = {
     repayment,
     investor_investment,
     active_loan,
-    auth_controll,
-    oauth2Facebook,
+    auth_controll
 };
